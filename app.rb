bd6a06155e8beebd4153c64091fb3de7a2aa7af3
albertweng13 <albertweng@berkeley.edu>
2016-02-08 22:21:24 +0000

    another fix for invalid guess

--- app.rb
+++ app.rb
@@ -40,7 +40,7 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    guess = false
+    guess = true
     begin
       guess = @game.guess(letter)
     rescue ArgumentError
