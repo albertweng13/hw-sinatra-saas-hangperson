73fee1cb2ebe3aeb7917d187ebb97ab78c1bdbff
albertweng13 <albertweng@berkeley.edu>
2016-02-08 21:56:57 +0000

    Full Game

--- app.rb
+++ app.rb
@@ -40,6 +40,13 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    result = @game.guess(letter)
+    if result == ArgumentError
+      flash[:message] = "Invalid guess"
+    elsif result == false
+      flash[:message] = "You have already used that letter."
+    end
+    
     redirect '/show'
   end
   
@@ -50,17 +57,32 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    status = @game.check_win_or_lose
+    if status == :win
+      redirect '/win'
+    elsif status == :lose
+      redirect '/lose'
+    end
+    
     erb :show # You may change/remove this line
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      erb :win
+    else
+      redirect '/show'
+    end
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose == :lose
+      erb :lose
+    else
+      redirect '/show'
+    end
   end
   
 end
